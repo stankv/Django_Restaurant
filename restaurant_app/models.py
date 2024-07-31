6fc8f90b9eae14de56acf4947b10a9fb618ad7fb
stankv <stankv@mail.ru>
2024-07-31 14:42:39 +0300

    Добавлена модель меню блюд

--- restaurant_app/models.py
+++ restaurant_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class MenuItem(models.Model):
+    title = models.CharField(max_length=70, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='images', verbose_name='Изображение')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена', default=0)
+
+    TYPE = [
+        ('BRK', 'Завтрак'),
+        ('LUN', 'Обед'),
+        ('DIN', 'Ужин'),
+    ]
+
+    type = models.CharField(choices=TYPE, max_length=3, default='BRK', verbose_name='Тип')
