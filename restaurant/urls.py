40aa9ae3336af6065e322fcbb6f7eb1adb4e24fb
stankv <stankv@mail.ru>
2024-08-02 13:45:47 +0300

    Добавлен маршрутизатор для главной страницы

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -19,8 +19,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from restaurant_app import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
 
 # подключение статических файлов (img, css, javascript для самого сайта)
