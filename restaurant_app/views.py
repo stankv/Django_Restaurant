7ec46a14e58994fa83c0f3896b7f9fea64810aec
stankv <stankv@mail.ru>
2024-08-02 13:19:25 +0300

    Добавлен контроллер для главной страницы index.html

--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from restasite.models import MenuItem
+
+
 # Create your views here.
+def index(request):
+    # считываем блюда из БД, сортируем случайным образом и выводим первые 6
+    menu_brk = MenuItem.objects.filter(type__exact='BRK').order_by('?')[:6]
+    menu_lun = MenuItem.objects.filter(type__exact='LUN').order_by('?')[:6]
+    menu_din = MenuItem.objects.filter(type__exact='DIN').order_by('?')[:6]
+    context = {'menu_brk': menu_brk, 'menu_lun': menu_lun, 'menu_din': menu_din}
+    return render(
+        request,
+        'index.html',
+        context=context
+    )
