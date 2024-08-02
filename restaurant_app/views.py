1b93e5d4e3bb91f93d74866961181776413bdaf4
stankv <stankv@mail.ru>
2024-08-02 18:09:08 +0300

    Изменен контроллер страницу контакты с учетом формы обратной связи

--- restaurant_app/views.py
+++ restaurant_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from restaurant_app.models import MenuItem
+from restaurant_app.forms import ContactForm
 
 
 # Create your views here.
@@ -25,7 +26,17 @@ def about(request):
 
 
 def contacts(request):
+    context = {}
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            send_message(form.cleaned_data['name'], form.cleaned_data['email'], form.cleaned_data['message'])
+            context = {'success': 1}
+    else:
+        form = ContactForm()
+    context['form'] = form
     return render(
         request,
-        'contacts.html'
+        'contacts.html',
+        context=context
     )
