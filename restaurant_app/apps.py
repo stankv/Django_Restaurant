d0c1dfa25a46764f910179f7afd7ccf3100237ec
stankv <stankv@mail.ru>
2024-08-21 16:22:07 +0300

    Изменено отображение названия приложения в адм.

--- restaurant_app/apps.py
+++ restaurant_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class RestaurantAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'restaurant_app'
+    verbose_name = "Ресторан"
