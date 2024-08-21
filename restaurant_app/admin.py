5e4d1a0bb1469563c0d06d97c4ca7853911b5725
stankv <stankv@mail.ru>
2024-08-21 16:24:07 +0300

    Добавлено отображение изобр-й блюд в админке

--- restaurant_app/admin.py
+++ restaurant_app/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from restaurant_app.models import MenuItem
 
 # Register your models here.
-admin.site.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+
+    # отображение изображений блюд в админке
+    @admin.display(description="Изображение")
+    def post_photo(self, photo: MenuItem):
+        if photo.image:
+            return mark_safe(f"<img src='{photo.image.url}' width=50>")
+        return "Без фото"
+
+    list_display = ['post_photo', 'type', 'title', 'description', 'price']
+    ordering = ['type']
+    readonly_fields = ['post_photo']
+
+admin.site.register(MenuItem, MenuItemAdmin)
